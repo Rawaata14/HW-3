c36468b82f57252e90776f198a30b3d5a2810935
Rawaata14 <rawatareef1@gmail.com>
2025-05-17 21:58:32 +0300

    Task#6

--- js.js
+++ js.js
@@ -1,4 +1,10 @@
-//Task#5
-const fruits = ["apple", "banana", "cherry", "date"];
-fruits.splice(1, 1, "blueberry", "blackberry");
-alert(fruits);
+// //Task#5
+// const fruits = ["apple", "banana", "cherry", "date"];
+// fruits.splice(1, 1, "blueberry", "blackberry");
+// alert(fruits);
+
+//Task#6
+const colors = ["red", "orange", "yellow", "green", "blue"];
+colors.splice(-2);
+colors.splice(0, 1);
+alert(colors);
