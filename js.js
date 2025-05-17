d3befbafc827611124974b9aec45d78b63759b38
Rawaata14 <rawatareef1@gmail.com>
2025-05-17 22:04:11 +0300

    Task#7

--- js.js
+++ js.js
@@ -3,8 +3,14 @@
 // fruits.splice(1, 1, "blueberry", "blackberry");
 // alert(fruits);
 
-//Task#6
-const colors = ["red", "orange", "yellow", "green", "blue"];
-colors.splice(-2);
-colors.splice(0, 1);
-alert(colors);
+// //Task#6
+// const colors = ["red", "orange", "yellow", "green", "blue"];
+// colors.splice(-2);
+// colors.splice(0, 1);
+// alert(colors);
+
+//Task#7
+const animals1 = ["cat", "dog"];
+const animals2 = ["lion", "tiger"];
+let animals3 = animals1.concat(animals2);
+alert(animals3);
